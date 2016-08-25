9c3803f5aa56de9c794fe0123d243180eaa5570c
rolenweb <rolenweb@yandex.ru>
2016-08-25 12:32:15 +0300

    del db

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ezaimexpert',
-    'username' => 'ezaimexpert',
-    'password' => 'R21L06en',
-    'charset' => 'utf8',
-];
