0f6d3e79006f274c87b1c3c007d9e75d56e80b6e
rolenweb <rolenweb@yandex.ru>
2016-09-27 18:07:00 +0300

    page error

--- basic/views/site/error.php
+++ basic/views/site/error.php
@@ -8,20 +8,29 @@
 use yii\helpers\Html;
 
 $this->title = $name;
+
+echo $this->render('_header',[
+    'city' => null,
+  ]);
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<section class="site-error">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+                <div class="alert alert-danger">
+                    <?= nl2br(Html::encode($message)) ?>
+                </div>
+
+                <p>
+                    The above error occurred while the Web server was processing your request.
+                </p>
+                <p>
+                    Please contact us if you think this is a server error. Thank you.
+                </p>    
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+</section>
